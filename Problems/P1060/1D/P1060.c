cfd33cb6c13e2edb6b4885939d9a715346655092
jerrykcode <jerrykang026@gmail.com>
2021-06-01 12:20:49 +0800

    Update P1060

--- Problems/P1060/1D/P1060.c
+++ Problems/P1060/1D/P1060.c
@@ -1,33 +1,22 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#ifndef max
-#define max(a, b) ((a) > (b) ? (a) : (b))
-#endif
+#define MAXN 30000
 
-typedef unsigned short word;
+int dp[MAXN + 1];
+
+int max(int a, int b) { return a > b ? a : b; }
 
 int main() {
     int n, m;
     scanf("%d %d", &n, &m);
-    int * dp = (int *)malloc((n + 1) * sizeof(int));
-    memset(dp, 0, (n + 1) * sizeof(int));
-    word * cost = (word *)malloc(m * sizeof(word));
-    word * value = (word *)malloc(m * sizeof(word));
+    int v, p;
     for (int i = 0; i < m; i++) {
-        scanf("%hd %hd", cost + i, value + i);
-        value[i] *= cost[i];
+        scanf("%d %d", &v, &p);
+        p *= v;
+        for (int j = n; j >= v; j--)
+            dp[j] = max(dp[j], dp[j - v] + p);
     }
-    //背包
-    for (int i = 0; i < m; i++)
-        for (int j = n; j >= cost[i]; j--) {
-            dp[j] = max(dp[j], dp[j - cost[i]] + value[i]);
-            if (i == m - 1) break;
-        }
-    printf("%d", dp[n]);
-    free(dp);
-    free(cost);
-    free(value);
-    return 0;
+    printf("%d\n", dp[n]);
+    exit(0);
 }
--- Problems/P1060/2D/P1060.c
+++ Problems/P1060/2D/P1060.c
@@ -1,46 +1,25 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#ifndef max
-#define max(a,b) (((a) > (b)) ? (a) : (b))
-#define min(a,b) (((a) < (b)) ? (a) : (b))
-#endif
+#define MAXM 24
+#define MAXN 30000
+
+int dp[MAXM + 1][MAXN + 1];
+
+int max(int a, int b) {return a > b ? a : b; }
 
 int main() {
-	int n, m;
-	scanf("%d %d", &n, &m);
-	int *cost = (int *)malloc((m + 1) * sizeof(int));
-	int *value = (int *)malloc((m + 1) * sizeof(int));
-	for (int i = 1; i < m + 1; i++) {
-		//输入i的价格和等级
-		scanf("%d %d", cost + i, value + i);
-		//i的价值为i的价格乘以i的等级
-		value[i] *= cost[i];
-	}
-	//初始化dp
-	int **dp = (int **)malloc((m + 1) * sizeof(int *));
-	for (int i = 0; i < m + 1; i++) {
-		dp[i] = (int *)malloc((n + 1) * sizeof(int));	
-		dp[i][0] = 0;
-	}
-	for (int i = 0; i < n + 1; i++) {
-		dp[0][i] = 0;
-	}
-	//背包
-	for (int i = 1; i < m + 1; i++) {
-		for (int j = 1; j < n + 1; j++) {
-			if (j >= cost[i]) {
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - cost[i]] + value[i]);
-			}
-			else dp[i][j] = dp[i - 1][j];
-		}
-	}
-	printf("%d", dp[m][n]);
-	//释放内存
-	free(cost);
-	free(value);
-	for (int i = 0; i < m + 1; i++)
-		free(dp[i]);
-	free(dp);
-	return 0;
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int v, p;
+    for (int i = 1; i <= m; i++) {
+        scanf("%d %d", &v, &p);
+        p *= v;
+        for (int j = 0; j < v; j++)
+            dp[i][j] = dp[i - 1][j];
+        for (int j = v; j <= n; j++)
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v] + p);
+    }
+    printf("%d\n", dp[m][n]);
+    exit(0);
 }
